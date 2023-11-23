920268cf57b7559bb987b3880f4c56d5dbea0ab8
Carolina <caroonava21@gmail.com>
2023-11-22 21:13:29 -0300

    funciones númericas

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 module.exports = () => {
   // ...
 };
+new Promise((resolve, reject) => {
+  
+})
+
+const calculadora = (pathRuta) => {
+  return new Promise((resolve, reject) => {
+    const reuslt = sumar(5,6)
+    const restado = restar(result, 2)
+    const dividido = dividir(restado, 3)
+    resolve(dividido)
+    resolve(isAbsolute(pathRuta))
+  })
+}
+
+calculadora('./readme.md')
+.then((res)=>console.log('aquí', res))
---
+++ src/functions.js
@@ -0,0 +1,23 @@
+//crear y modular funciones especificas antes de hacer la promesa
+//ej funcion 'readFile'- 'transformar la ruta en absoluta" - 'comprobar que la ruta exista'
+
+export const sumar = (n1, n2) => {
+    return n1+2
+}
+
+const restar = (n1, n2) => {
+    return n1 - n2
+}
+
+const validarN2 = (n2) => {
+    return n2!==0 ? true  : false
+}
+const dividir = (n1, n2) => {
+    validarN2(n2)
+    return n1 /n2
+}
+
+const isAbsolute = (ruta) => {
+fdsfsdfsfs
+return 
+}
