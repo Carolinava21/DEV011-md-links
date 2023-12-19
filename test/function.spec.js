224c315efcd873e763ca1f213c731527180f0dbd
Carolina <caroonava21@gmail.com>
2023-12-18 23:23:14 -0300

    some tests

--- test/function.spec.js
+++ test/function.spec.js
@@ -1,8 +1,13 @@
+const path = require('path');
+const fs = require('fs')
 const { isAbsolutePath, 
 convertAbsolute,
 verifyPathExistence,
 isItMarkdown,
+linksExtract,
 validateLinks,
+readingFile,
+statsFunction
  } = require ('../src/function.js');
 
 describe ("isAbsolutePath", () => {
@@ -36,8 +41,76 @@ describe ("isItMarkdown?",() => {
   });
 });
 
-// describe("validateLinks", () => {
-//   it("return true if the links are valid, including an array",() => {
-//     const valid
-//   })
-// })
+//reading test
+describe("readingFile", () => {
+  it("the content of the file is being read", () => {
+    const testPath = './links.md';
+    return readingFile(testPath)
+      .then(fileContent => {
+        expect(typeof fileContent).toBe('string');
+      });
+  });
+});
+//extract links
+describe("extractLinks", () => {
+  it("the links are extracted, and an array is returned", () => {
+    const testPath = './links.md';
+    return fs.readFile(testPath, 'utf-8', (err, fileContent) => {
+      if (err) throw err;
+
+      const linkArray = [
+        {
+          href: 'https://www.youtube.com/watch?v=4JXnTxXg5sI',
+          text: 'lofixmas',
+          file: 'links.md'
+        },
+        {
+          href: 'https://developer.mozilla.org/es/docs/Web/HTTP/Status',
+          text: 'httpstatus',
+          file: 'links.md'
+        },
+        {
+          href: 'https://luisrrleal.com/blog/como-hacer-peticiones-http-en-javascript',
+          text: 'información',
+          file: 'links.md'
+        },
+        {
+          href: 'https://www.bookbub.com/blog/free-short-stories-online',
+          text: 'stories',
+        },
+        {
+          href: 'https://www.grammarly.com/',
+          text: 'grammarly',
+          file: 'links.md'
+        },
+        {
+          href: 'https://dictionary.cambridge.org/es/diccionario/',
+          text: 'diccionario',
+          file: 'links.md'
+        },
+        
+      ];
+
+      expect(linksExtract(fileContent, testPath)).toEqual(linkArray);
+    });
+  });
+});
+
+describe('statsFunction', () => {
+  it('stats shows the total and Unique links inside the file', () => {
+    const links = [
+      { href: 'https://www.youtube.com/watch?v=4JXnTxXg5sI' },
+      { href: 'https://www.grammarly.com/' }
+    ];
+    const stats = statsFunction(links);
+    expect(stats).toEqual({ Total: links.length, Unique: links.length });
+  });
+});
+
+// //validate links 
+// describe("validateLinks" , () => {
+//   it( "the links are validated" 
+//   )
+// }
+// ) 
+
