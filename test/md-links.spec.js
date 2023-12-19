f030332d9d2cb433409e2ed7541ed6b8c5aac9d8
Carolina <caroonava21@gmail.com>
2023-12-19 00:20:03 -0300

    mdlinks tests

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,17 +1,36 @@
 const mdLinks = require('../src/index.js');
-const axios = require('axios');
-
 
 describe('mdLinks', () => {
 
-  it('should return a promise', () => {
+  it('it should return a promise', () => {
     expect(mdLinks('docs/05-milestone.md')).toBeInstanceOf(Promise);
 });
   });
-
   
-  jest.mock('axios');// usar spy on
+  it('it should return an array with the links properties', () => {
+    return mdLinks('links.md')
+    .then((result) => {
+      expect(Array.isArray(result)).toBe(true);
+      result.forEach((link) => {
+        expect(link).toHaveProperty('href');
+        expect(link).toHaveProperty('text'); 
+      });
+    });
+  });
+  it('it should return the array of links when stats is set to false', () => {
+    return mdLinks('README.md', { stats: false })
+      .then((result) => {
+        expect(Array.isArray(result)).toBe(true); 
+      });
+  });
 
- 
-
-
+  it('it should the validate links', () => {
+    const linkPath = 'links.md';
+    return mdLinks(linkPath, true).then((result) => {
+      result.forEach((link) => {
+        expect(link).toHaveProperty('href');
+        expect(link).toHaveProperty('text');
+        expect(link).toHaveProperty('file');
+      });
+    });
+  });
